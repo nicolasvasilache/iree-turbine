94279629d8eba50632feb2306899b19cee884da1
Nicolas Vasilache <nico.vasilache@amd.com>
2025-06-24 23:38:34 +0200

    Notes re custom op registration and generation

---
+++ sandbox/nico/notes.py
@@ -0,0 +1,19 @@
+"""
+iree-turbine/iree/turbine/ops/insert_slice.py :: l 53 registers custom op
+
+1. custom op works like a regular Python PT op, specified by
+
+    signature = (
+        "insert_slice(Tensor src, Tensor dst, int[] offset, int[] strides) -> (Tensor)"
+    )
+
+2. generates whatever thanks to `def generate`
+
+3. variant selected by `def select`
+
+This was meant to be the entry point for tkl to connect to the `def generate`.
+
+Could also connect to instrinsics to get ggml-style Q5.
+
+Cross custom-op mechanism is better discussed through sharktank (@1:02:00)
+"""
